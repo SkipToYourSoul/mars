a548a35a057bac195388893960ab2fc05e132fc3
sophia <sophia@sophiatekiMacBook-Pro.local>
2013-08-23 15:42:03 +0800

    Remove handshake handler

--- src/main/java/com/zeedoo/mars/server/ServerChannelInitializer.java
+++ src/main/java/com/zeedoo/mars/server/ServerChannelInitializer.java
@@ -9,12 +9,10 @@ import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
 import com.zeedoo.mars.server.handler.InboundSunMessageHandler;
-import com.zeedoo.mars.server.handler.HandshakeHandler;
 
 /**
  * Creates a newly configured {@link ChannelPipeline} for a new channel.
@@ -27,25 +25,8 @@ public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> 
 		
 	private static final StringDecoder DECODER = new StringDecoder(CharsetUtil.UTF_8);
 	
-	//TODO: Make this JMX persistent
-	//TODO: Use real handshake once we're there
-	private boolean skipHandshake = true;
-	
 	@Autowired
 	private InboundSunMessageHandler inboundSunMessageHandler;
-	
-	@Autowired
-	private HandshakeHandler handshakeHandler;
-	
-	@ManagedAttribute(description = "Whether the initial handshake is skipped upon the connection establishment with a Sun device")
-	public boolean isSkipHandshake() {
-		return skipHandshake;
-	}
-
-	@ManagedAttribute(description = "Sets whether the initial handshake is skipped upon the connection establishment with a Sun device")
-	public void setSkipHandshake(boolean skipHandshake) {
-		this.skipHandshake = skipHandshake;
-	}
 
 	@Override
 	public void initChannel(SocketChannel ch) throws Exception {
@@ -60,13 +41,7 @@ public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> 
 		// and then business logic
 		pipeline.addLast("messageDecoder", new MessageDecoder());
 		pipeline.addLast("messageEncoder", new MessageEncoder());
-		
-		if (skipHandshake) {
-			LOGGER.info("Handshake on initial connection is DISABLED. Skipping adding handshake handler");
-		} else{
-			// Add handshake handler
-			pipeline.addLast("handshakeHandler", handshakeHandler);
-		}
+
 		pipeline.addLast("inboundSunMesageHandler", inboundSunMessageHandler);
 	}
 }
